376e7098e5be4aeed4b6956fa9921e24faa00dc1
Larry Lenox <larrylenx9@gmail.com>
2025-02-26 20:27:31 +0200

    edit settings

--- horchen/settings.py
+++ horchen/settings.py
@@ -93,13 +93,14 @@ STATICFILES_DIRS = [
 ]
 
 # Static files
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "myapp", "static", "myapp")]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
